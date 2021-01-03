469615ba24e6fc74b569e6ea3dfbc0c30bdf2d58
Hillel Garmi <hillel.garmi@gmail.com>
2021-01-04 00:16:42 +0200

    private version

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+let lang;
+
+const index = window.location.href.indexOf("?");
+if(index != -1){
+    lang = window.location.href.substr(index + 6, 2);
+}
+
+console.log(lang);
+
+if(lang != "he"){
+    lang = "en"
+}
+
+console.log(options[lang]);
+
+var data = options[lang]
+
 var app = new Vue({
     el: '#app',
-    data: {
-        title: 'Coming Soon',
-        subtitle: "Very Soon!"
+    data: data,
+    methods: {
     }
 })
+
+function login() {
+    if(data.message == "DontRuinTheScoop"){
+        data.enabled = true;
+    }
+}
---
+++ options.js
@@ -0,0 +1,150 @@
+const options = {
+    en: {
+        enabled: false,
+        password: "",
+        bootstrap_link: {
+            href: "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
+            integrity: "sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2",
+        },
+        donate: true,
+        title_lines: [
+            "2020",
+            "Shministim Letter",
+        ],
+        sub_title: "We are calling for high school seniors (shministiyot) our age to ask themselves: What and who are we serving when we enlist in the military? Why do we enlist? What reality do we create by serving in the military of the occupation? We want peace, and real peace requires justice. Justice requires acknowledgment of the historical and present injustices, and of the continuing Nakba. Justice requires reform in the form of the end of the occupation, the end of the siege on Gaza, and recognition of the right of return for Palestinian refugees. Justice demands solidarity, joint struggle, and refusal. ",
+        sections: [
+            {
+                id: "top",
+                heading: "The Top"
+            },
+            {
+                id: "letter",
+                heading: "The Entire Letter"
+            },
+            {
+                id: "solidarity",
+                heading: "Solidarity Decleration"
+            },
+            {
+                id: "donate",
+                heading: "Donate"
+            },
+            {
+                id: "moreLinks",
+                heading: "More Links"
+            },
+        ],
+        full_letter: [
+            // {p: "yo"},
+            {p: "We are a group of Israeli 18-year-olds at a crossroads. The Israeli state is demanding our conscription into the military. Allegedly, a defense force which is supposed to safeguard the existence of the State of Israel. In reality, the goal of the Israeli military is not to defend itself from hostile militaries, but to exercise control over a civilian population. In other words, our conscription to the Israeli military has political context and implications. It has implications, first and foremost on the lives of the Palestinian people who have lived under violent occupation for 72 years. Indeed, the Zionist policy of brutal violence towards and expulsion of Palestinians from their homes and lands began in 1948 and has not stopped since. The occupation is also poisoning Israeli society–it is violent, militaristic, oppressive, and chauvinistic. It is our duty to oppose this destructive reality by uniting our struggles and refusing to serve these violent systems–chief among them the military. Our refusal to enlist to the military is not an act of turning our backs on Israeli society. On the contrary, our refusal is an act of taking responsibility over our actions and their repercussions. "},
+            {p: "The military is not only serving the occupation, the military is the occupation. Pilots, intelligence units, bureaucratic clerks, combat soldiers, all are executing the occupation. One does it with a keyboard and the other with a machine gun at a checkpoint. Despite all of this, we grew up in the shadow of the symbolic ideal of the heroic soldier. We prepared food baskets for him in the high holidays, we visited the tank he fought in, we pretended we were him in the pre-military programs in high school, and we revered his death on memorial day. The fact that we are all accustomed to this reality does not make it apolitical. Enlistment, no less than refusal, is a political act. "},
+            {p: "We are used to hearing that it is legitimate to criticize the occupation only if we took an active part in enforcing it. How does it make sense that in order to protest against systemic violence and racism, we have to first be part of the very system of oppression we are criticizing? "},
+            {p: "The track upon which we embark at infancy, of an education teaching violence and claims over land, reaches its peak at age 18, with the enlistment in the military. We are ordered to put on the bloodstained military uniform and preserve the legacy of the Nakba and of occupation. Israeli society has been built upon these rotten roots, and it is apparent in all facets of life: in the racism, the hateful political discourse, the police brutality, and more. "},
+            {p: "This military oppression goes hand in hand with economic oppression. While the citizens of the Occupied Palestinian Territories are impoverished, wealthy elites become richer at their expense. Palestinian workers are systematically exploited, and the weapons industry uses the Occupied Palestinian Territories as a testing ground and as a showcase to bolster its sales. When the government chooses to uphold the occupation, it is acting against our interest as citizens– large portions of taxpayer money is funding the “security” industry and the development of settlements instead of welfare, education, and health. "},
+            {p: "The military is a violent, corrupt, and corrupting institution to the core. But its worst crime is enforcing the destructive policy of the occupation of Palestine. Young people our age are required to take part in enforcing closures as a means of “collective punishment,” arresting and jailing minors, blackmailing to recruit “collaborators” and more– all of these are war crimes which are executed and covered up every day. Violent military rule in the Occupied Palestinian Territories is enforced through policies of apartheid entailing two different legal systems: one for Palestinians and the other for Jews. The Palestinians are constantly faced with undemocratic and violent measures, while Jewish settlers who commit violent crimes– first and foremost against Palestinians but also against soldiers- are “rewarded” by the Israeli military turning a blind eye and covering up these transgressions. The military has been enforcing a siege on Gaza for over ten years. This siege has created a massive humanitarian crisis in the Gaza Strip and is one of the main factors which perpetuates the cycle of violence of Israel and Hamas. Because of the siege, there is no drinkable water nor electricity in Gaza for most hours of the day. Unemployment and poverty are pervasive and the healthcare system lacks the most basic means. This reality serves as the foundation on top of which  the disaster of COVID-19 has only made things worse in Gaza."},
+            {p: "It is important to emphasize that these injustices are not a one-time slippage or straying away from the path. These injustices are not a mistake or a symptom, they are the policy and the disease. The actions of the Israeli military in 2020 are nothing but a continuation and upholding of the legacy of massacre, expulsion of families, and land theft, the legacy which “enabled” the establishment of the State of Israel, as a proper democratic state, for Jews only. "},
+            {p: "Historically, the military has been seen as a tool which serves the “melting pot” policy, as an institution which crosscuts social class and gender divides in Israeli society. In reality, this could not be further from the truth. The military is enacting a clear program of ‘channeling’; soldiers from upper-middle class are channelled into positions with economic and civilian prospects, while soldiers from lower socioeconomic backgrounds are channelled into positions which have high mental and physical risk and which do not provide the same head start in civil society. Simultaneously, women’s representation in violent positions such as pilots, tank commanders, combat soldiers, and intelligence officers, is being marketed as feminist achievment. How does it make sense that the struggle against gender inequality is achieved through the oppression of Palestinian women? These “achievements” sidestep solidarity with the struggle of Palestinian women. The military is cementing these power relations and the oppression of marginalized communities through a cynical co-opting of their struggles. "},
+            {p: "We are calling for high school seniors (shministiyot) our age to ask themselves: What and who are we serving when we enlist in the military? Why do we enlist? What reality do we create by serving in the military of the occupation? We want peace, and real peace requires justice. Justice requires acknowledgment of the historical and present injustices, and of the continuing Nakba. Justice requires reform in the form of the end of the occupation, the end of the siege on Gaza, and recognition of the right of return for Palestinian refugees. Justice demands solidarity, joint struggle, and refusal. "},
+        ],
+        solidarity: "In refusing to serve in the Israeli military, the Shministiyot (Israeli high schoolers) who wrote this letter are refusing to normalize Israeli settler colonialism. Above all, this is an act of solidarity with the Palestinian people who live under the Israeli occupation, as well as with Palestinian refugees and their descendants in the exile. This is also an act of deep care for the society they live in. A society which refuses to see its ingrained injustices, which glorifies militarism, which is coopted to its core into the global industrial military complex. Like the Americans who demand to abolish the police, like the Indian farmers who strike against their dispossession by corporations, like the Lebanese who call for a fundamental change in sectarian rule and neoliberal policies, and like many other peoples across the globe, the Shministiyot are seeking to create a new common sense. A common sense in which solidarity is prevalent and injustice is condemned. A common sense which normalizes the fact that systems of oppression are run by people and can be dismantled by people. Join the Shminitiyot in their struggle against Israeli settler colonialism and occupation of Palestine, and show them your support by signing below.",
+        more_links: [
+            {
+                title: "First Slide",
+                subtitle: "First subtitle",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "First Slide",
+                id: "carousel-item-1",
+            },
+            {
+                title: "Second Slide",
+                subtitle: "Second subtitle",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "Second Slide",
+                id: "carousel-item-2",
+            },
+            {
+                title: "Third Slide",
+                subtitle: "Third subtitle",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "Third Slide",
+                id: "carousel-item-3",
+            },
+        ]
+    },
+    he: {
+        enabled: false,
+        password: "",
+        bootstrap_link: {
+            href: "https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css",
+            integrity: "sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe",
+        },
+        donate: false,
+        solidarity: false,
+        title_lines: [
+            "מכתב השמיניסטים",
+            "2020",
+        ],
+        sub_title: "אנחנו קוראות לשמיניסטיות בנות גילנו לשאול את עצמן, את מה ומי אנחנו משרתות בגיוס שלנו? למה בעצם אנחנו מתגייסות? איזו מציאות אנחנו מייצרות כשאנחנו משרתות בצבא הכיבוש? אנחנו רוצות שלום, ושלום אמיתי דורש צדק. צדק דורש הכרה בעוולות העבר וההווה, והנכבה המתמשכת. צדק דורש תיקון בצורת סיום הכיבוש, הסרת המצור על עזה והכרה בזכות השיבה של הפליטים הפלסטינים. צדק דורש סולידריות, מאבק משותף וסירוב.",
+        sections: [
+            {
+                id: "top",
+                heading: "ראש הדף"
+            },
+            {
+                id: "letter",
+                heading: "המכתב המלא"
+            },
+            {
+                id: "moreLinks",
+                heading: "קישורים נוספים"
+            },
+        ],
+        full_letter: [
+            {p: "לכבוד"},
+            {p: "מר בנימין נתניהו, ראש הממשלה"},
+            {p: 'מר אביב כוכבי, הרמטכ"ל'},
+            {p: 'ח"כ שר הביטחון בני גנץ'},
+            {p: 'ח"כ שר החינוך יואב גלנט'},
+            {p: 'אנחנו בני ובנות 18 העומדות בפני פרשת דרכים. המדינה דורשת מאיתנו להתגייס. לכאורה, לצבא הגנה, שנועד להבטיח את קיומה של מדינת ישראל. אבל בפועל פעילות הצבא הישראלי אינה מכוונת בעיקרה להגנה מפני צבאות אויב, אלא לשליטה על אוכלוסייה אזרחית. כלומר, לגיוס שלנו יש הקשר והשלכות. בראש ובראשונה על חיי העם הפלסטיני שחי תחת כיבוש אלים במשך 72 שנים. מדיניות האלימות וגירוש הפלסטינים מאדמותיהם ובתיהם התחילה ב-1948 ומאז לא עצרה לרגע. הכיבוש גם מרעיל את החברה הישראלית - היא אלימה, מיליטריסטית, מדכאת ושוביניסטית. חובתנו להתנגד למציאות הרסנית זו על ידי איחוד מאבקים וסירוב לשרת את אותן המערכות האלימות - ובראשן הצבא. הסירוב שלנו להתגייס הוא לא אקט של ניתוק או התכחשות לחברה הישראלית אלא להפך- לקיחת אחריות על המעשים שלנו וההשלכות שלהם.'},
+            {p: 'אנחנו רגילות לשמוע שלגיטימי לבקר את הכיבוש רק אם לקחנו בו חלק פעיל. איך זה הגיוני שכדי למחות נגד אלימות וגזענות מערכתית, אנחנו קודם צריכות להיות חלק מאותה מערכת מדכאת?'},
+            {p: 'הצבא לא רק משרת את הכיבוש, הצבא הוא הכיבוש. טייסים, חיילי 8200, פקידי הבירוקרטיה, לוחמים, כולם מוציאים לפועל את הכיבוש, זה בעזרת מקלדת, וזה עם נשק במחסום. אף על פי כן, גדלנו בצילו של אידיאל החייל הגיבור. הכנו לו משלוחי מנות, ביקרנו את הטנק שהוא לחם בו, התחזנו אליו בגדנ"ע והאדרנו את מותו ביום הזיכרון. העובדה שזו מציאות שכולנו רגילים אליה לא הופכת אותה לְאַ-פוליטית. גיוס הוא פוליטי, לא פחות מסירוב.'},
+            {p: 'המסלול עליו אנחנו עולים בגיל אפס, של חינוך לאלימות ובעלות על הארץ, מגיע לשיאו בגיל 18, עם גיוסנו לצבא. דורשים מאיתנו לעטות את מדי הצבא המגואלים בדם ולהמשיך את מורשת הנכבה והכיבוש. על יסודות רקובים אלה נבנית החברה הישראלית וזה משתקף בכל- בגזענות, בשיח הפוליטי המסית, באלימות המשטרתית ועוד.'},
+            {p: 'הדיכוי הצבאי הולך יד ביד עם הדיכוי הכלכלי. בעוד תושבי השטחים הכבושים מתרוששים, בעלי ההון מתעשרים על חשבונם. פועלים פלסטיניים מנוצלים באופן שיטתי, ותעשיית הנשק משתמשת בשטחים הכבושים כשטח ניסויים וחלון ראווה להגברת מכירות. כשהממשלה בוחרת לשמר את הכיבוש, היא עושה זאת גם למען אותם בעלי הון. כשהיא עושה זאת, היא פועלת נגד האינטרסים שלנו - חלקים גדולים מכספי המיסים של אזרחי ישראל הולכים למימון תעשיית הביטחון ופיתוח התנחלויות במקום לרווחה, לחינוך ולבריאות.'},
+            {p: 'הצבא הוא גוף אלים, מושחת ומשחית מיסודו, אך הרעה הגדולה שהוא אוכף מתחילה במדיניות הכיבוש ההרסנית. צעירים וצעירות בגילנו נדרשות לקחת חלק באכיפת סגרים שהם אמצעי ענישה קולקטיבים, במעצר קטינים, במניעת טיפול רפואי, בסחיטה לשם גיוס "משתפי פעולה" ועוד – כל אלו הם פשעי מלחמה המבוצעים ומטוייחים מדי יום. המשטר הצבאי האלים בשטחים הכבושים משליט מדיניות אפרטהייד המתבטאת בשתי מערכות חוק שונות; מערכת אחת לפלסטינים ושנייה ליהודים. הפלסטינים חשופים דרך קבע לאמצעים אלימים ולא דמוקרטיים. לעומת זאת, פשעים אלימים שמבצעים מתנחלים - בראש ובראשונה נגד פלסטינים אך גם נגד החיילים עצמם ״זוכים״ להעלמת עין ולטיוח מצד המשטר הצבאי ומדינת ישראל. הצבא מטיל מצור על עזה מעל עשר שנים. מצור זה יצר משבר הומניטרי חריף ברצועה והוא אחד הגורמים העיקריים המנציחים את מעגל האלימות של ישראל מול חמאס. בעקבות המצור אין בעזה מים ראויים לשתייה וחשמל ברוב שעות היממה, ישנם אחוזים גבוהים של אבטלה ועוני ולמערכת הבריאות חסרים האמצעים הבסיסיים ביותר. מציאות זו מהווה תשתית לאסון הקורונה שהולך ומחמיר בעזה.'},
+            {p: 'חשוב להדגיש שהעוולות הללו אינן מעידה חד פעמית או סטייה מהתלם. הן אינן טעות או סימפטום, הן המדיניות והמחלה. פעילות הצבא הישראלי ב-2020 אינה אלא המשך ותחזוק המורשת של הטבח ההמוני, גירוש המשפחות, ונישול האדמות ש"בזכותה" קמה מדינת ישראל, מדינה מתוקנת ודמוקרטית, ליהודים בלבד.'},
+            {p: 'היסטורית, הצבא נתפס ככלי המשרת את מדיניות כור ההיתוך, כמוסד המערער על המעמדות החברתיים והמגדריים בישראל. בפועל, המציאות לא יכולה להיות יותר רחוקה מזה. הצבא מקיים הסללה ברורה; חיילים מהמעמד הבינוני-גבוה מוצבים בתפקידים שיש בהם עתיד כלכלי ואזרחי, בעוד שחיילים מהפריפריה החברתית מוצבים בתפקידים שהסיכון הפיזי והנפשי בהם הוא גבוה יותר ולא יתורגמו לקידום באזרחות. במקביל, משווקת לנו תפיסה של פמיניזם כייצוג נשי בתפקידים כוחניים כמו טייסות, מפקדות טנקים, לוחמות וקצינות מודיעין. איך יתכן שהמאבק נגד אי-שוויון מגדרי יעשה דרך הדיכוי האלים של נשים פלסטיניות? ה״הישגים״ האלה באים על חשבון סולידריות עם מאבקן של הנשים הפלסטיניות. הצבא מנציח את יחסי הכוחות הללו ואת הדיכוי של אוכלוסיות מוחלשות תוך ניצול ציני של מאבקן.'},
+            {p: 'אנחנו קוראות לשמיניסטיות בנות גילנו לשאול את עצמן, את מה ומי אנחנו משרתות בגיוס שלנו? למה בעצם אנחנו מתגייסות? איזו מציאות אנחנו מייצרות כשאנחנו משרתות בצבא הכיבוש? אנחנו רוצות שלום, ושלום אמיתי דורש צדק. צדק דורש הכרה בעוולות העבר וההווה, והנכבה המתמשכת. צדק דורש תיקון בצורת סיום הכיבוש, הסרת המצור על עזה והכרה בזכות השיבה של הפליטים הפלסטינים. צדק דורש סולידריות, מאבק משותף וסירוב.'},
+            {p: ''},
+            
+        ],
+        more_links: [
+            {
+                title: "קישור ראשון",
+                subtitle: "כותרת משנה ראשונה",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "First Slide",
+                id: "carousel-item-1",
+            },
+            {
+                title: "קישור שני",
+                subtitle: "כותרת משנה שנייה",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "Second Slide",
+                id: "carousel-item-2",
+            },
+            {
+                title: "קישור שלישי",
+                subtitle: "כותרת משנה שלישית",
+                link: "https://getbootstrap.com/docs/4.0/components/carousel/",
+                png: "placeholder.png",
+                alt: "Third Slide",
+                id: "carousel-item-3",
+            },
+        ]
+    }
+}
